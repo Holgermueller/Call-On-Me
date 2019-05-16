c0545ccc2aee5637fdcf43f139441947e660b8b0
Holger Mueller <9jholger@gmail.com>
2019-05-16 16:17:26 -0500

    began controllers

---
+++ controllers/instructorController.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+const instructor = require("../models/instructors");
+const classes = require("../models/classes");
+const student = require("../models/students");
+
+module.exports = {
+  register_instructor: (req, res) => {},
+
+  login_instructor: (req, res) => {},
+
+  create_class: (req, res) => {},
+
+  delete_class: (req, res) => {},
+
+  add_student_to_class: (req, res) => {},
+
+  edit_student: (req, res) => {},
+
+  get_all_students: (req, res) => {},
+
+  delete_student: (req, res) => {}
+};
