a7cc651e701d4a5a040bb6432b992a8544842209
Holger Mueller <9jholger@gmail.com>
2019-06-24 19:54:30 -0500

    added config file, fixed proxy error

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const studentRoutes = require("./studentAPI");
 
 router.use("/instructorapi", instructorRoutes);
 router.use("/classapi", classRoutes);
-router.use("/student", studentRoutes);
+router.use("/studentapi", studentRoutes);
 
 router.use((req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
